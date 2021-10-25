5a50fe5e723b840dbdb622606cdcbacfd0bfe8a7
afrederiksen <dev@adamfrederiksen.com>
2021-10-25 10:06:19 -0400

    move out of research folder

---
+++ backend/src/test/java/com/adamfrederiksen/research/ResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfrederiksen.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
