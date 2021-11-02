31d47b921b2ac9171fd2278b98e53e95e842115d
afrederiksen <dev@adamfrederiksen.com>
2021-11-02 01:44:09 -0400

    Created Recipe Object & Disabled Spring Boot Security
    Recipe object has parameters. default values are for testing purposes. Needs to be converted to DTO (for SQL integration)
    @SpringBootApplication(exclude = SecurityAutoConfiguration.class) in the ResearchApplication
    Also refactored code to be Fridger-related

---
+++ backend/src/main/java/recipes/fridger/backend/Recipe.java
@@ -0,0 +1,86 @@
+package recipes.fridger.backend;
+
+import java.util.List;
+
+public class Recipe {
+    private final long recipeId;
+    private final String title;
+    private final String author;
+    private final String description;
+    private final Integer totalTime;
+    private final Integer prepTime;
+    private final Integer cookTime;
+    private final Integer yield;
+    private final List<String> ingredients;
+    private final float estimatedCost;
+    private final String type;
+    private final boolean alcoholic;
+    private final String[] tags;
+    private final float rating;
+
+    public Recipe(long recipeId, String title, String author, String description, Integer totalTime, Integer prepTime, Integer cookTime, Integer yield, List<String> ingredients, float estimatedCost, String type, boolean alcoholic, String[] tags, float rating) {
+        this.recipeId = recipeId;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.totalTime = totalTime;
+        this.prepTime = prepTime;
+        this.cookTime = cookTime;
+        this.yield = yield;
+        this.ingredients = ingredients;
+        this.estimatedCost = estimatedCost;
+        this.type = type;
+        this.alcoholic = alcoholic;
+        this.tags = tags;
+        this.rating = rating;
+    }
+
+    public long getRecipeId() {
+        return recipeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getTotalTime() {
+        return totalTime;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public Integer getYield() {
+        return yield;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public float getEstimatedCost() {
+        return estimatedCost;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isAlcoholic() {
+        return alcoholic;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+}
+
