d3d461a8b4073d022e86f4ab0a0c5b7633d0cb24
afrederiksen <dev@adamfrederiksen.com>
2021-11-13 03:27:02 -0500

    Cleaned up CSS and did revised routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,3 @@
-import {
-  IonApp,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonMenuToggle,
-  IonPage,
-  IonButtons,
-  IonButton,
-  IonIcon,
-} from '@ionic/react';
 
 // import { FaHome, FaBars, FaUtensils, FaAppleAlt, FaUser, FaHamburger, FaFolder, FaHeart, FaCog} from 'react-icons/fa';
 
@@ -44,53 +32,25 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import SideBar from './components/SideBar';
-import RecipeCard from './components/RecipeCard';
-import { menuOutline } from 'ionicons/icons';
 
 //const App: React.FC = () => (
   function App () {
     return (
+    <>
   <Router history={history}>
     <Switch>
-      <Route path="/Home" component={Home} />
-      <Route path="/Recipes" component={Recipes} />
-      <Route path="/Ingredients" component={Ingredients} />
-      <Route path="/GoalsPage" component={GoalsPage} />
-      <Route path="/myPantry" component={myPantry} />
-      <Route path="/myReviews" component={myReviews} />
+      <Route path="/recipes" component={Recipes} />
+      <Route path="/ingredients" component={Ingredients} />
+      <Route path="/goals" component={GoalsPage} />
+      <Route path="/mypantry" component={myPantry} />
+      <Route path="/myreviews" component={myReviews} />
       <Route path="/favorites" component={Favorites} />
       <Route path="/preferences" component={Preferences} />
-  <IonApp>
-    <SideBar />
-    <IonPage className="ion-page" id="main-content">
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuToggle>
-              <IonButton>
-                <IonIcon icon={menuOutline} slot="start" />
-              </IonButton>
-            </IonMenuToggle>
-          </IonButtons>
-          <IonTitle>Fridger</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>Welcome back, Seongho!</h1>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-
-
-        
-      </IonContent>
-    </IonPage>
-  </IonApp>
+      <Route path="/" component={Home} />
   </Switch>
   </Router>
+  </>
 );
-
 }
 
 
--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -2,41 +2,14 @@ import './Recipes.css';
 
 import {
     IonApp,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
     IonContent,
-    IonMenuToggle,
     IonPage,
-    IonButtons,
-    IonButton,
-    IonIcon,
   } from '@ionic/react';
 
-import { Router, Switch, Route } from "react-router-dom";
+import { Router, Switch } from "react-router-dom";
 import history from '../History';
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import '../theme/variables.css';
 import SideBar from '../components/SideBar';
-import { menuOutline } from 'ionicons/icons';
-
+import Header from '../components/Header';
 
 function Recipes() {
     return (
@@ -45,18 +18,7 @@ function Recipes() {
                 <IonApp>
     <SideBar />
     <IonPage className="ion-page" id="main-content">
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuToggle>
-              <IonButton>
-                <IonIcon icon={menuOutline} slot="start" />
-              </IonButton>
-            </IonMenuToggle>
-          </IonButtons>
-          <IonTitle>Fridger</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header/>
       <IonContent className="ion-padding">
         <h1>Search for a recipe, or browse! Coming soon!</h1>
       </IonContent>
