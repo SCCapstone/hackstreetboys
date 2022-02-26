adb70321b5a5eb711327849754c452656db155af
jtschuler <jtschuler007@yahoo.com>
2022-02-26 16:56:07 -0500

    registration form now functional

--- frontend/src/models/User.ts
+++ frontend/src/models/User.ts
@@ -7,9 +7,3 @@ export interface User {
     height_in: number;
     weight_lb: number;
 }
-
-/*
- * TODO add user profie picture both here and in backend, probably just a path
- * to where the file is stored. To that end we'll have to come up with a naming
- * scheme and locations for any images users upload.
- */
