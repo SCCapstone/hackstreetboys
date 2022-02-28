fb7cab40776840eeaeb9df2532f60e7bd2168953
Andrew-Bernhardt <77554911+Andrew-Bernhardt@users.noreply.github.com>
2022-02-27 22:28:19 -0500

    Update FridgerApplication.java
    this wasn't supposed to be in

--- backend/src/main/java/recipes/fridger/backend/FridgerApplication.java
+++ backend/src/main/java/recipes/fridger/backend/FridgerApplication.java
@@ -9,20 +9,11 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import recipes.fridger.backend.mail.EmailService;
 
-@Slf4j
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FridgerApplication {
-	@Autowired
-	private EmailService emailService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FridgerApplication.class, args);
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void sendMail() {
-		emailService.sendSimpleEmail("andrew.bernhardt@fridger.recipes","Hello! This is a test for fridger!","do ya thing dog");
-		log.info("sent test hopefully");
 	}
 
 }
