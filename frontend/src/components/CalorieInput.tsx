50f1b1432479a0affecc52885e126c585ea93b30
afrederiksen <dev@adamfrederiksen.com>
2022-04-14 00:27:09 -0400

    Inputs have labels now

--- frontend/src/components/CalorieInput.tsx
+++ frontend/src/components/CalorieInput.tsx
@@ -1,18 +1,22 @@
-import { IonButton, IonInput } from '@ionic/react';
+import { IonButton, IonInput, IonLabel } from '@ionic/react';
 import { values } from 'lodash';
 import React from 'react';
 
-const CalorieInput = ({addItemHandler, calories, mealName, setCalories, setItemName}: any) => {
+const CalorieInput = ({ addItemHandler, calories, mealName, setCalories, setItemName }: any) => {
     const onAddItemClick = () => {
         addItemHandler();
     }
 
     return (
         <div>
-            <IonInput color = 'dark' type="text" placeholder="Food Item" value={mealName} onIonInput={(e: any) => setItemName(e.target.value)}/>
-              <IonInput color='dark' type="number" placeholder='Calories'value={calories} onIonInput={(e: any) => setCalories(e.target.value)}/>
-              <IonButton onClick={onAddItemClick} color="dark">Add Item</IonButton>
+            <br />
+            <IonLabel position="floating">Food Item</IonLabel>
+            <IonInput type="text" placeholder="Enter Food Item" value={mealName} onIonInput={(e: any) => setItemName(e.target.value)} />
+            <IonLabel position="floating">Calories</IonLabel>
+            <IonInput type="number" placeholder='Enter Calories' value={calories} onIonInput={(e: any) => setCalories(e.target.value)} />
+            <IonButton onClick={onAddItemClick} color="dark">Add Item</IonButton>
         </div>
     );
 }
+// style={{ maxHeight:'250px', width:'100%', objectFit: 'cover'}}
 export default CalorieInput;
