70eea5b6c9ad919c0ff354bf4ae9ae702b3bea05
jtschuler <jtschuler007@yahoo.com>
2022-04-15 09:24:58 -0400

    context fix and quality of life updates

--- frontend/src/components/LogInOrProfileLink.tsx
+++ frontend/src/components/LogInOrProfileLink.tsx
@@ -35,7 +35,7 @@ export const LogInOrProfileLink: React.FC = () => {
     }
 
     return (
-        <IonButton color='secondary' routerLink={`/register`} slot="end">
+        <IonButton color='secondary' routerLink={`/login`} slot="end">
         Log In
     </IonButton>
     );
